d57f4bc559765b9760701b18dcc6ecc42f81b79e
Tim Schumacher <timschumi@gmx.de>
2020-11-17 13:36:58 +0100

    irc: Remove empty lines from message before sending
    Signed-off-by: Tim Schumacher <timschumi@gmx.de>

--- bridge/irc/irc.go
+++ bridge/irc/irc.go
@@ -220,6 +220,18 @@ func (b *Birc) createPaste(content string) string {
 	return result.Paste
 }
 
+func (b *Birc) RemoveEmpty(lines []string) []string {
+	var out []string
+
+	for _, l := range lines {
+		if l != "" {
+			out = append(out, l)
+		}
+	}
+
+	return out
+}
+
 func (b *Birc) Send(msg config.Message) (string, error) {
 	// ignore delete messages
 	if msg.Event == config.EventMsgDelete ||
@@ -282,6 +294,8 @@ func (b *Birc) Send(msg config.Message) (string, error) {
 	} else {
 		msgLines = helper.GetSubLines(msg.Text, 0, b.GetString("MessageClipped"))
 	}
+
+	msgLines = b.RemoveEmpty(msgLines)
 
 	originalText := msg.Text
 
