5fff1b26e91c5c483c4175a9ffc42e9f25e41b67
Tim Schumacher <timschumi@gmx.de>
2020-11-17 13:43:20 +0100

    irc: Control long message pastes seperately
    Signed-off-by: Tim Schumacher <timschumi@gmx.de>

--- bridge/irc/irc.go
+++ bridge/irc/irc.go
@@ -40,7 +40,7 @@ type Birc struct {
 
 	PasteMinLines, PastePreviewLines   int
 	PasteDomain, PasteAPI, PasteAPIKey string
-	PasteCodeblocks                    bool
+	PasteCodeblocks, PasteLongMessages bool
 
 	*bridge.Config
 }
@@ -83,6 +83,7 @@ func New(cfg *bridge.Config) bridge.Bridger {
 	}
 	b.PasteAPIKey = b.GetString("PasteAPIKey")
 	b.PasteCodeblocks = b.GetBool("PasteCodeblocks")
+	b.PasteLongMessages = b.GetBool("PasteLongMessages")
 	b.FirstConnection = true
 	return b
 }
@@ -305,7 +306,8 @@ func (b *Birc) Send(msg config.Message) (string, error) {
 			return "fake-id", nil
 		}
 
-		if b.PasteDomain != "" && len(msgLines) >= b.PasteMinLines && i >= b.PastePreviewLines {
+		if b.PasteDomain != "" && b.PasteLongMessages &&
+			len(msgLines) >= b.PasteMinLines && i >= b.PastePreviewLines {
 			var link string
 			link = b.createPaste(originalText)
 
